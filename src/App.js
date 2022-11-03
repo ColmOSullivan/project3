1a07db54d97adc77f885a413cc4ead79f3cb2785
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-11-02 22:00:38 -0400

    Moved components into folder, styled button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import './Setup.css';
-import Header from './Header';
-import GenreForm from './GenreForm';
-import Footer from './Footer'
+import Header from './components/Header';
+import GenreForm from './components/GenreForm';
+import Footer from './components/Footer'
 
 
 
